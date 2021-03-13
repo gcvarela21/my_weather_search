38fee5d06899d73f1d4727cc2d5b94fb3a7daba3
Gloria Varela <gvarela2@horizon.csueastbay.edu>
2021-03-12 21:42:55 -0800

    re-organized the html index, still working on the button. we can still console log the pre-set search of the city of orange

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-const APIKey = "cdf85afeac2c889cd209181450dfe03d";
-function getWeather(cityName) {
+// var citySearch = $("#city-search");
+// var cityHistory = $(".city-history");
+// var cityArr = [];
+// var cityName = $("#city-name");
+// var dateToday = $("#date");
+// var weatherToday = $("#weather-today");
+// var cityTemp = $("#temp");
+// var humidity = $("#humidity");
+// var windSpeed = $("#wind-speed");
+// var UVIndex = $("#UVIndex");
+// var fiveDay = $("#fiveDay");
+// var mainCol = $(".main-weather");
+
+
+
+// search bar
+
+
+// $("#submit-button").on("click", function(event) {
+// });
+ 
+
+
+function getWeather() {
+    const APIKey = "cdf85afeac2c889cd209181450dfe03d";
+    // var cityName = document.getElementById("city-search").value;
+   var cityName = "orange";
 
 let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
     $.ajax({
@@ -7,8 +32,58 @@ let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName +
          method: "GET"
         }).then(function(response){
         console.log(response);
+
         });
     };
 
-getWeather("orange");
+getWeather();
 console.log("we searchin");
+
+
+
+// function search(event) {
+//     event.preventDefault();
+    
+//     document.querySelector(".main-column").hidden = false;
+
+//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch.val() + "&units=imperial&appid=cdf85afeac2c889cd209181450dfe03d"
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         console.log(response);
+//         console.log(queryURL);
+//         addHistory(citySearch.val());
+//         showWeatherToday(response);
+//         weatherWeek(response);
+//         UVIndex.removeClass("good mod bad")
+//     });
+// }
+
+
+// function addHistory(cityToSave) {
+//     cityArr.push(cityToSave);
+//     localStorage.setItem("cityHistory", JSON.stringify(cityArr));
+//     showHistory();
+// }
+
+
+// function showHistory() {
+//     var cityList = JSON.parse(localStorage.getItem("cityHistory"))
+//     var div = $("<div>")
+//     var button = $("<button>")
+//     button.addClass("btn history-btn btn-outline-primary")
+//     button.text(cityList[cityList.length - 1]);
+//     div.append(button)
+//     $(".city-storage").prepend(div);
+
+// }
+
+// $(".history-btn").on("click", function(event) {
+//     event.preventDefault();
+//     search(event);
+// })
+
+
+
